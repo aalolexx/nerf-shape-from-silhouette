02e09f213080a8676568441982557305ebffe275
Alex Ehrenhöfer <ehren.alex@web.de>
2024-05-14 22:43:17 +0200

    Image Segmentation - Added Closing filters

--- pipeline_steps/ImageBinarizer.py
+++ pipeline_steps/ImageBinarizer.py
@@ -5,27 +5,32 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 from PIL import Image
 import numpy as np
 import os
+import cv2
 
 class ImageBinarizer:
-    def __init__(self, images_raw_path: str, images_segmented_path: str) -> None:
+    def __init__(self, images_raw_path: str,
+                 images_segmented_path: str,
+                 closing_kernel_size: int = 5) -> None:
         self._images_raw_path = images_raw_path
         self._images_segmented_path = images_segmented_path
+        self._closing_kernel_size = closing_kernel_size
 
     def __call__(self, context: Context, next_step: NextStep) -> None:
-        #Prepare Sam Image Segmentation Model
+        # Prepare Sam Image Segmentation Model
         sam = sam_model_registry["default"](checkpoint="data/misc/sam_vit_h_4b8939.pth")
         mask_generator = SamAutomaticMaskGenerator(sam)
+
+        closing_kernel = np.ones((self._closing_kernel_size, self._closing_kernel_size), np.uint8)
 
         os.makedirs(self._images_segmented_path, exist_ok=True)
 
         # Loop trough all images
         for filename in os.listdir(self._images_raw_path):
             if filename.endswith(('.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG')):
-                i += 1
                 input_image_path = os.path.join(self._images_raw_path, filename)
                 output_image_path = os.path.join(self._images_segmented_path, f"mask_{filename}")
 
-                #todo aeh resize image
+                # todo aeh resize image
 
                 # Load Image
                 input_image = Image.open(input_image_path)
@@ -37,6 +42,12 @@ class ImageBinarizer:
 
                 # Convert to binary representation (foreground as white, background as black)
                 binary_mask[binary_mask == 1] = 255
+                binary_mask = binary_mask.astype(np.uint8)
+
+                # Close small error holes
+                binary_mask = cv2.dilate(binary_mask, closing_kernel, iterations=1)
+                binary_mask = cv2.erode(binary_mask, closing_kernel, iterations=1)
+
                 binary_image = Image.fromarray(binary_mask.astype('uint8'), 'L')
 
                 # Save the mask image
