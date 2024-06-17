8fe550d4bdd2b287cfa7758748c00abde44b2962
Alex Ehrenhöfer <ehren.alex@web.de>
2024-06-17 12:00:06 +0200

    fixed custom silhouete model confs

--- alex-silhouette-model/alex_silhouette_model/custom_config.py
+++ alex-silhouette-model/alex_silhouette_model/custom_config.py
@@ -1,4 +1,8 @@
+from nerfstudio.data.datamanagers.parallel_datamanager import ParallelDataManagerConfig
+from nerfstudio.data.dataparsers.dnerf_dataparser import DNeRFDataParserConfig
 from nerfstudio.engine.trainer import TrainerConfig
+from nerfstudio.models.nerfacto import NerfactoModelConfig
+from nerfstudio.pipelines.base_pipeline import VanillaPipeline, VanillaPipelineConfig
 from nerfstudio.plugins.types import MethodSpecification
 
 from alex_silhouette_model.custom_datamanager import (
@@ -27,7 +31,7 @@ alex_silhouette_model = MethodSpecification(
     steps_per_save=2000,
     max_num_iterations=30000,
     mixed_precision=True,
-    pipeline=CustomPipelineConfig(
+      pipeline=CustomPipelineConfig(
         datamanager=CustomDataManagerConfig(
             dataparser=CustomDataParserConfig(),
             train_num_rays_per_batch=4096,
@@ -35,6 +39,7 @@ alex_silhouette_model = MethodSpecification(
         ),
         model=CustomModelConfig(
             eval_num_rays_per_chunk=1 << 15,
+            average_init_density=0.01
         ),
     ),
     optimizers={
@@ -43,8 +48,8 @@ alex_silhouette_model = MethodSpecification(
             "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
         },
         "fields": {
-            "optimizer": RAdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=50000),
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=200000),
         },
         "camera_opt": {
            "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
